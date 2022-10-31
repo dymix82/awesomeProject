7b4bbaa121004d7119c0abb2ea589b45706b0bac
dymix82 <d.gavva@gmail.com>
2022-11-01 00:36:54 +0300

    задание 8

--- main.go
+++ main.go
@@ -5,17 +5,44 @@ import (
 )
 
 func main() {
-	fmt.Println("Введие число 1:")
+	fmt.Println("Введие зарплату 1")
 	var a int
 	fmt.Scan(&a)
-	fmt.Println("Введие число 2:")
+	fmt.Println("Введие зарплату 2:")
 	var b int
 	fmt.Scan(&b)
-	var result int
-	result = a % b
-	if result == 0 {
-		fmt.Println("Число делится без остатка")
-	} else {
-		fmt.Println("Число делится с остатком")
+	fmt.Println("Введие зарплату 3:")
+	var c int
+	fmt.Scan(&c)
+	var min int
+	var mid int
+	var max int
+	if a < b && b < c {
+		min = a
+		mid = b
+		max = c
+	} else if b < a && a < c {
+		min = b
+		mid = a
+		max = c
+	} else if c < a && a < b {
+		min = c
+		mid = a
+		max = b
+	} else if b < c && c < a {
+		min = b
+		mid = c
+		max = a
+	} else if c < b && b < a {
+		min = c
+		mid = b
+		max = a
+	} else if a < c && c < b {
+		min = a
+		mid = c
+		max = b
 	}
+	fmt.Println(min, mid, max)
+	fmt.Println("Pазница между самой большой и самой маленькой зарплатой:", max-min)
+	fmt.Println("Средняя зарплата:", (max+mid+min)/3)
 }
