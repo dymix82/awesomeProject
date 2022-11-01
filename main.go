8bdbfcafdd8897fd61b6affc82e366fca76c52f8
dymix82 <d.gavva@gmail.com>
2022-11-01 08:44:30 +0300

    задание 1

--- main.go
+++ main.go
@@ -5,44 +5,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Введие зарплату 1")
+	fmt.Println("Введите IQ космонавта  1")
 	var a int
 	fmt.Scan(&a)
-	fmt.Println("Введие зарплату 2:")
+	fmt.Println("Введите IQ космонавта  2:")
 	var b int
 	fmt.Scan(&b)
-	fmt.Println("Введие зарплату 3:")
+	fmt.Println("Введите IQ космонавта  3:")
 	var c int
 	fmt.Scan(&c)
-	var min int
-	var mid int
 	var max int
-	if a < b && b < c {
-		min = a
-		mid = b
+	if a < c && b < c {
 		max = c
-	} else if b < a && a < c {
-		min = b
-		mid = a
-		max = c
-	} else if c < a && a < b {
-		min = c
-		mid = a
-		max = b
-	} else if b < c && c < a {
-		min = b
-		mid = c
+		fmt.Print("Капитан коробля стал космонафт №3 с IQ: ", max)
+	} else if b < a && c < a {
 		max = a
-	} else if c < b && b < a {
-		min = c
-		mid = b
-		max = a
-	} else if a < c && c < b {
-		min = a
-		mid = c
+		fmt.Print("Капитан коробля стал космонафт №1 с IQ: ", max)
+	} else if c < b && a < b {
 		max = b
+		fmt.Print("Капитан коробля стал космонафт №2 с IQ: ", max)
 	}
 	// fmt.Println(min, mid, max)
-	fmt.Println("Pазница между самой большой и самой маленькой зарплатой:", max-min)
-	fmt.Println("Средняя зарплата:", (max+mid+min)/3)
+
 }
