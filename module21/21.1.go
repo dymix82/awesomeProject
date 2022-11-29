de23a09b391c5f66c037e20832af6d899a49392b
dymix82 <d.gavva@gmail.com>
2022-11-29 14:05:18 +0300

    задание 21

--- module21/21.1.go
+++ module21/21.1.go
@@ -7,3 +7,24 @@
 //Тип S должен быть во float32.
 
 package main
+
+import "fmt"
+
+var (
+	S float32
+	x int16
+	y uint8
+	z float32
+)
+
+func main() {
+	x = 4                                            // задаем значение х
+	y = 4                                            // задаем значение y
+	z = 1.56                                         // задаем значение z
+	F := func(x int16, y uint8, z float32) float32 { // Описываем анонимную функцию для нахождения  S = 2 × x + y ^ 2 − 3/z
+		S = float32(2*x) + float32(y*y) - (3 / z)
+		return S
+	}
+	S = F(x, y, z) // Вызываем функцию со значениями x,y,z
+	fmt.Println(S) // Выводим результат
+}
