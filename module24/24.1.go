8b098797a69e9709a604857208282d92f1601bd1
dymix82 <d.gavva@gmail.com>
2022-12-04 14:41:31 +0300

    задание 24

--- module24/24.1.go
+++ module24/24.1.go
@@ -3,6 +3,35 @@
 // Напишите функцию, сортирующую массив длины 10 вставками.
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+const size = 10
+
+func main() {
+	ar := make([]int, size)
+	for k := 0; k < len(ar); k++ { // Наполняем массив
+		rand.Seed(time.Now().UnixNano())
+		time.Sleep(2)
+		x := rand.Intn(20)
+		//		fmt.Println(x)
+		ar[k] = x
+	}
+	InsertionSort(ar)
+	fmt.Println("Отсортированный массив:")
+	fmt.Println(ar)
+}
+
+func InsertionSort(ar []int) {
+	for i := 1; i < len(ar); i++ {
+		x := ar[i]
+		j := i
+		for ; j >= 1 && ar[j-1] > x; j-- {
+			ar[j] = ar[j-1]
+		}
+		ar[j] = x
+	}
 }
