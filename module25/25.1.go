5c3af95fe9323271130a0c4bbf222407d247813c
dymix82 <d.gavva@gmail.com>
2022-12-06 22:52:31 +0300

    задание 25

---
+++ module25/25.1.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string
+	var substr string
+	flag.StringVar(&str, "str", "null", "str")
+	flag.StringVar(&substr, "substr", "null", "substr")
+	flag.Parse()
+	fmt.Println(strings.Contains(str, substr))
+
+}
