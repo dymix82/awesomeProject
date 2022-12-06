a3417ed7670f49c4fa2e212ea5fbe43d4e6c2610
dymix82 <d.gavva@gmail.com>
2022-12-07 00:45:58 +0300

    задание 25

--- module25/25.1.go
+++ module25/25.1.go
@@ -1,9 +1,14 @@
+// Цель задания
+// Написать программу для нахождения подстроки в кириллической подстроке.
+// Программа должна запускаться с помощью команды:
+// go run main.go --str "строка для поиска" --substr "поиска"
+// Для реализации такой работы с флагами воспользуйтесь пакетом flags,
+// а для поиска подстроки в строке вам понадобятся руны.
 package main
 
 import (
 	"flag"
 	"fmt"
-	"strings"
 )
 
 func main() {
@@ -12,5 +17,21 @@ func main() {
 	flag.StringVar(&str, "str", "null", "str")
 	flag.StringVar(&substr, "substr", "null", "substr")
 	flag.Parse()
-	fmt.Println(strings.Contains(str, substr))
+	strRunes := []rune(str)
+	substrRunes := []rune(substr)
+	fmt.Println(stringSearch(strRunes, substrRunes))
+}
+func stringSearch(strRunes []rune, substrRunes []rune) bool {
+	j := 0
+	for _, r := range strRunes {
+		if r == substrRunes[j] {
+			j++
+			if j == len(substrRunes) {
+				return true
+			}
+		} else {
+			j = 0
+		}
+	}
+	return false
 }
