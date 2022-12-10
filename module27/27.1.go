85ebf754fa16d6159d212d96cc517e0deecec8f2
dymix82 <d.gavva@gmail.com>
2022-12-10 10:57:02 +0300

    задание 27

--- module27/27.1.go
+++ module27/27.1.go
@@ -17,25 +17,15 @@ type Student struct {
 	grade int
 }
 
-//	func newPerson(name string) *person {
-//		p := person{name: name}
-//		p.age = 42
-//		return &p
-//	}
+func newStudent(name string, age int, grade int) Student {
+	p := Student{name: name,
+		age:   age,
+		grade: grade}
+	return p
+}
 func main() {
 	StudentName := make(map[int]Student)
-	fmt.Println("Введите студенов построчно в формате имя возраст курс  через пробел")
-	//	stdin, err := io.ReadAll(os.Stdin)
-	//	if err != nil {
-	//
-	//	}
-	//	str := string(stdin)
-	//	words := strings.Fields(str)
-	//	for i,v:= range words {
-	//		if
-	//	}
-	//
-	//	fmt.Println(words)
+	fmt.Println("Введите студенов построчно в формате имя возраст оценку через пробел, оценку от 1 до 5")
 	index := 1
 	for {
 		reader := bufio.NewReader(os.Stdin)
@@ -44,16 +34,34 @@ func main() {
 
 		//	fmt.Println(text)
 		s := strings.Split(text, " ")
+		//	for _, v := range s {
+		//		fmt.Println(v)
+		//	}
 		if err == io.EOF {
 			fmt.Println(StudentName)
 			goto label
 		}
-		age, _ := strconv.Atoi(s[1])
-		grade, _ := strconv.Atoi(s[2])
-		print(s[2])
-		sp := Student{name: s[0], age: age, grade: grade}
-		StudentName[index] = sp
-		fmt.Println(StudentName)
+		age, err2 := strconv.Atoi(s[1])
+		if err2 != nil {
+			fmt.Println("Ошибка при вводе возроста, введите строку еще раз")
+			fmt.Println("Введите студенов построчно в формате имя возраст оценку через пробел, оценку от 1 до 5")
+			continue
+		}
+		grade, err3 := strconv.Atoi(s[2])
+		if err3 != nil {
+			fmt.Println("Ошибка при вводе оценки, введите строку еще раз")
+			fmt.Println("Введите студенов построчно в формате имя возраст оценку через пробел, оценку от 1 до 5")
+			continue
+		}
+		if grade < 0 || grade > 5 {
+			fmt.Println("Ошибка при вводе оценки, введите строку еще раз")
+			fmt.Println("Введите студенов построчно в формате имя возраст оценку через пробел, оценку от 1 до 5")
+			continue
+		}
+		//	print(s[2])
+		//	sp := Student{name: s[0], age: age, grade: grade}
+		StudentName[index] = newStudent(s[0], age, grade)
+		fmt.Println("Студент добавлен")
 		index++
 	}
 label:
