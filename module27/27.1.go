7c4c22f575ce9c7d0dbb24215eb4b944cadd1dcf
dymix82 <d.gavva@gmail.com>
2022-12-10 17:45:53 +0300

    задание 27

--- module27/27.1.go
+++ module27/27.1.go
@@ -17,21 +17,25 @@ type Student struct { // Описываем структуру
 	grade int
 }
 
-var StudentName = make(map[int]*Student)
+var StudentName = make(map[string]*Student)
 
-func newStudent(name string, age int, grade int) *Student { //Функция добавления данных в структуру
-	p := Student{name: name,
-		age:   age,
-		grade: grade}
-	return &p
-}
-func (s Student) get() string {
+// func newStudent(name string, age int, grade int) *Student { //Функция добавления данных в структуру
+//
+//		p := Student{name: name,
+//			age:   age,
+//			grade: grade}
+//		return &p
+//	}
+func (s Student) get() string { // Метод get
 	return s.name + " " + strconv.Itoa(s.age) + " " + strconv.Itoa(s.grade)
+}
+func (s *Student) put(m map[string]*Student) { // Метод put
+	m[s.name] = s
 }
 
 func main() {
 	fmt.Println("Введите студентов построчно в формате имя возраст курс через пробел, курс от 1 до 5")
-	index := 1
+	//	index := 1
 	for {
 		reader := bufio.NewReader(os.Stdin)
 		text, err := reader.ReadString('\n')
@@ -65,8 +69,9 @@ func main() {
 			fmt.Println("Введите студентов построчно в формате имя возраст курс через пробел, курс от 1 до 5")
 			continue
 		}
-		StudentName[index] = newStudent(s[0], age, grade) // Добавляем функцией студента
+		student := Student{s[0], age, grade}
+		student.put(StudentName)
 		fmt.Println("Студент добавлен")
-		index++
+		//	index++
 	}
 }
