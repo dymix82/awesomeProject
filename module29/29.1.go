9fd6712c44b58c4cdc6c4fe4f3559b725e1f1add
dymix82 <d.gavva@gmail.com>
2022-12-15 23:35:52 +0300

    задание 29

--- module29/29.1.go
+++ module29/29.1.go
@@ -1,1 +1,40 @@
-package module29
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var input string
+var sqr int
+
+func main() {
+	ch1 := make(chan int)
+	fmt.Println("Введите число , если хотите остановиться введите 'стоп'")
+	for {
+		fmt.Scan(&input)
+		//	wg.Add(2)
+		if input == "стоп" {
+			break
+		}
+		value, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Это не число, попробуйте ввести число еще раз")
+		}
+		go sqrt(value, ch1)
+		value2 := <-ch1
+		fmt.Println(value2)
+		go multiplyByTwo(value2, ch1)
+		fmt.Println(<-ch1)
+	}
+
+}
+
+func multiplyByTwo(num int, out chan<- int) {
+	result := num * 2
+	out <- result
+}
+func sqrt(num int, out chan<- int) {
+	result := num * num
+	out <- result
+}
